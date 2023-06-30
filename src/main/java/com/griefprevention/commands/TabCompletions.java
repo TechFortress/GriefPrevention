762c640bd5c4fc7a9854da7772bd0d5a81a98853
Jikoo <jikoo.games@gmail.com>
2023-06-30 12:23:27 -0400

    Use Bukkit's startsWithIgnoreCase, document

--- src/main/java/com/griefprevention/commands/TabCompletions.java
+++ src/main/java/com/griefprevention/commands/TabCompletions.java
@@ -3,6 +3,7 @@ package com.griefprevention.commands;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -18,7 +19,7 @@ final class TabCompletions
 {
 
     /**
-     * Offer tab completions for visible players' names.
+     * Offer completions for visible players' names.
      *
      * @param sender the sender
      * @param args the existing command arguments
@@ -26,44 +27,54 @@ final class TabCompletions
      */
     static @NotNull List<String> visiblePlayers(@Nullable CommandSender sender, @NotNull String @NotNull [] args)
     {
+        // Bukkit returns a view of the player list. So that Craftbukkit doesn't have to hack around type limitations,
+        // this is actually a view of the player implementation, represented via Bukkit as a generic extending Player.
+        // Unfortunately, this leads to our own type limitations. We can work around those by converting to an array,
+        // which has the side benefit of allowing us to support every other data type with the same generic method
+        // instead of having to have otherwise-identical array and iterable methods.
         Player[] onlinePlayers = Bukkit.getOnlinePlayers().toArray(new Player[0]);
+        // Require sender to be able to see a player to complete their name.
         Predicate<Player> canSee = sender instanceof Player senderPlayer ? senderPlayer::canSee : null;
-        return complete(onlinePlayers, Player::getName, canSee, lastArg(args));
+        return complete(onlinePlayers, Player::getName, canSee, args);
     }
 
+    /**
+     * Offer completions matching an array of options. Options can be filtered.
+     *
+     * @param completable the array of completable options
+     * @param asString the method for converting an option to a String
+     * @param filter the filter to apply, or null for no filtering
+     * @param args the existing command arguments
+     * @return a {@link List} of all matching completable options in {@code String} form
+     * @param <T> the type of the option
+     */
     private static <T> @NotNull List<String> complete(
             T @NotNull [] completable,
             @NotNull Function<T, String> asString,
             @Nullable Predicate<T> filter,
-            @Nullable String prefix)
+            @NotNull String @NotNull [] args)
     {
+        // Length should never be 0 because that case should be handled by Bukkit completing the raw command name.
+        String prefix = args.length == 0 ? "" : args[args.length - 1];
+
         List<String> completions = new ArrayList<>();
 
         for (T element : completable)
         {
+            // Require element to be present and match filter, if provided.
             if (element == null || filter != null && !filter.test(element)) continue;
 
             String string = asString.apply(element);
-            if (string != null && startsWithIgnoreCase(string, prefix))
+            // Require element string to be non-empty and start with user's existing text.
+            if (string != null
+                    && !string.isEmpty()
+                    && (prefix.isEmpty() || StringUtil.startsWithIgnoreCase(string, prefix)))
             {
                 completions.add(string);
             }
         }
 
         return completions;
-    }
-
-    private static @Nullable String lastArg(@NotNull String @NotNull [] args)
-    {
-        if (args.length < 1) return null;
-        return args[args.length - 1];
-    }
-
-    private static boolean startsWithIgnoreCase(@NotNull String string, @Nullable String prefix)
-    {
-        if (prefix == null || prefix.isEmpty()) return true;
-        if (prefix.length() > string.length()) return false;
-        return string.regionMatches(true, 0, prefix, 0, prefix.length());
     }
 
     private TabCompletions() {}
