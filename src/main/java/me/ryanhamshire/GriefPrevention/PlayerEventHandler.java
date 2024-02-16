64d6d5e247e02939fb2c057d29ecbd6c16890738
QarthO <qartho@gmail.com>
2024-02-16 11:49:55 -0500

    updated GRASS to SHORT_GRASS

--- src/main/java/me/ryanhamshire/GriefPrevention/PlayerEventHandler.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/PlayerEventHandler.java
@@ -2177,7 +2177,7 @@ class PlayerEventHandler implements Listener
                 }
                 else
                 {
-                    allowedFillBlocks.add(Material.GRASS);
+                    allowedFillBlocks.add(Material.SHORT_GRASS);
                     allowedFillBlocks.add(Material.DIRT);
                     allowedFillBlocks.add(Material.STONE);
                     allowedFillBlocks.add(Material.SAND);
@@ -2241,7 +2241,7 @@ class PlayerEventHandler implements Listener
                             }
 
                             //only replace air, spilling water, snow, long grass
-                            if (block.getType() == Material.AIR || block.getType() == Material.SNOW || (block.getType() == Material.WATER && ((Levelled) block.getBlockData()).getLevel() != 0) || block.getType() == Material.GRASS)
+                            if (block.getType() == Material.AIR || block.getType() == Material.SNOW || (block.getType() == Material.WATER && ((Levelled) block.getBlockData()).getLevel() != 0) || block.getType() == Material.SHORT_GRASS)
                             {
                                 //if the top level, always use the default filler picked above
                                 if (y == maxHeight)
@@ -2653,7 +2653,7 @@ class PlayerEventHandler implements Listener
             Material type = result.getType();
             if (type != Material.AIR &&
                     (!passThroughWater || type != Material.WATER) &&
-                    type != Material.GRASS &&
+                    type != Material.SHORT_GRASS &&
                     type != Material.SNOW) return result;
         }
 
