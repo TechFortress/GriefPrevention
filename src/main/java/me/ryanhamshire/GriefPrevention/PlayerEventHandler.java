012a1afd29abc123f957a8daed1a51cd450e0240
QarthO <qartho@gmail.com>
2024-02-17 07:23:17 -0500

    supports both before and after the GRASS -> SHORT_GRASS change in mc 1.20.3+

--- src/main/java/me/ryanhamshire/GriefPrevention/PlayerEventHandler.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/PlayerEventHandler.java
@@ -2149,7 +2149,15 @@ class PlayerEventHandler implements Listener
                 }
                 else
                 {
-                    allowedFillBlocks.add(Material.GRASS);
+
+                    // in Minecraft 1.20.3 GRASS was renamed to SHORT_GRASS
+                    // This will support both before/after the change
+                    try{
+                        allowedFillBlocks.add(Material.valueOf("GRASS"));
+                    } catch(IllegalArgumentException e){
+                        allowedFillBlocks.add(Material.valueOf("SHORT_GRASS"));
+                    }
+
                     allowedFillBlocks.add(Material.DIRT);
                     allowedFillBlocks.add(Material.STONE);
                     allowedFillBlocks.add(Material.SAND);
@@ -2212,8 +2220,17 @@ class PlayerEventHandler implements Listener
                                 break;
                             }
 
+                            // in Minecraft 1.20.3 GRASS was renamed to SHORT_GRASS
+                            // This will support both before/after the change
+                            Material grassMaterial;
+                            try{
+                                grassMaterial = Material.valueOf("GRASS");
+                            } catch(IllegalArgumentException e){
+                                grassMaterial = Material.valueOf("SHORT_GRASS");
+                            }
+
                             //only replace air, spilling water, snow, long grass
-                            if (block.getType() == Material.AIR || block.getType() == Material.SNOW || (block.getType() == Material.WATER && ((Levelled) block.getBlockData()).getLevel() != 0) || block.getType() == Material.GRASS)
+                            if (block.getType() == Material.AIR || block.getType() == Material.SNOW || (block.getType() == Material.WATER && ((Levelled) block.getBlockData()).getLevel() != 0) || block.getType() == grassMaterial)
                             {
                                 //if the top level, always use the default filler picked above
                                 if (y == maxHeight)
