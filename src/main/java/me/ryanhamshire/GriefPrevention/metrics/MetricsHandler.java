ce949c9c0621fefcb35205031ced19de4a51da49
Jikoo <jikoo.games@gmail.com>
2024-03-05 13:36:49 -0500

    No bar graphs

--- src/main/java/me/ryanhamshire/GriefPrevention/metrics/MetricsHandler.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/metrics/MetricsHandler.java
@@ -5,8 +5,6 @@ import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.World;
 
 import java.util.concurrent.Callable;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Created on 9/22/2018.
@@ -110,13 +108,17 @@ public class MetricsHandler
         //siege
         addSimplePie("uses_siege", !plugin.config_siege_enabledWorlds.isEmpty());
 
-        metrics.addCustomChart(new Metrics.SimpleBarChart(
-                "claim_modes",
-                () -> plugin.config_claims_worldModes.values().stream()
-                        .filter(mode -> mode != null && mode != ClaimsMode.Disabled)
-                        .distinct()
-                        .map(Enum::name)
-                        .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(v -> 1)))));
+        // Which claims modes are actually used?
+        addSimplePie(
+                "uses_claims_survival",
+                () -> String.valueOf(plugin.config_claims_worldModes.values().stream().anyMatch(mode -> mode == ClaimsMode.Survival)));
+        addSimplePie(
+                "uses_claims_survival_req_claim",
+                () -> String.valueOf(plugin.config_claims_worldModes.values().stream().anyMatch(mode -> mode == ClaimsMode.SurvivalRequiringClaims)));
+        addSimplePie(
+                "uses_claims_creative",
+                () -> String.valueOf(plugin.config_claims_worldModes.values().stream().anyMatch(mode -> mode == ClaimsMode.Creative)));
+
     }
 
     private void addSimplePie(String id, boolean value)
@@ -126,13 +128,18 @@ public class MetricsHandler
 
     private void addSimplePie(String id, String value)
     {
-        metrics.addCustomChart(new Metrics.SimplePie(id, new Callable<String>()
+        addSimplePie(id, new Callable<String>()
         {
             @Override
             public String call() throws Exception
             {
                 return value;
             }
-        }));
+        });
+    }
+
+    private void addSimplePie(String id, Callable<String> callable)
+    {
+        metrics.addCustomChart(new Metrics.SimplePie(id, callable));
     }
 }
