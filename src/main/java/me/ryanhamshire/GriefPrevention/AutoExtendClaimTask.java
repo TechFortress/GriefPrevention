d590dc3b24c59d32cfa8bcc15ade317c02214a89
Jikoo <jikoo.games@gmail.com>
2024-03-15 14:16:53 -0400

    Ignore trash blocks
    This should ideally help prevent some issues with the main reason for having claims extend down instead of snapping to the bottom immediately, namely player mineshafts.

--- src/main/java/me/ryanhamshire/GriefPrevention/AutoExtendClaimTask.java
+++ src/main/java/me/ryanhamshire/GriefPrevention/AutoExtendClaimTask.java
@@ -174,7 +174,12 @@ public class AutoExtendClaimTask implements Runnable
 
     private Set<Material> getBiomePlayerBlocks(Biome biome)
     {
-        return biomePlayerMaterials.computeIfAbsent(biome, newBiome -> RestoreNatureProcessingTask.getPlayerBlocks(this.worldType, newBiome));
+        return biomePlayerMaterials.computeIfAbsent(biome, newBiome ->
+                {
+                    Set<Material> playerBlocks = RestoreNatureProcessingTask.getPlayerBlocks(this.worldType, newBiome);
+                    playerBlocks.removeAll(BlockEventHandler.TRASH_BLOCKS);
+                    return playerBlocks;
+                });
     }
 
     //runs in the main execution thread, where it can safely change claims and save those changes
